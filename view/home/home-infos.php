293040c6f574015f9acd26b520f05f929d761f31
chris_tophe <nehligchris@live.fr>
2023-05-27 02:21:58 +0200

    fix flexslider et début infos services

--- view/home/home-infos.php
+++ view/home/home-infos.php
@@ -1,11 +1,18 @@
-<?php global $all; global $infos_logo; ?>
+<?php global $all; global $metaHome; global $svg;
+
+
+
+debug($metaHome);
+debug($svg);
+debug($all);
+?>
 
 <div id="infos">
     <div class="head-wrap">
         <div class="header-infos">
             <img src="<?php echo asset('img/bg-green-white.svg'); ?>" alt="logo MD Com&Sport">
 
-            <h1 class="title head-infos">MD Com&Sport</h1>
+            <h1 class="title head-infos"><?php echo $metaHome['nom_marque'][0]; ?></h1>
 
             <img src="<?php echo asset('img/bg-green-white.svg'); ?>" alt="logo MD Com&Sport">
         </div>
@@ -15,5 +22,44 @@
         
     <div class="wrap">
         <h2 class="title">Qui suis-je ?</h2>
+
+        <div class="infos-container">
+            <div class="infos-left">
+                <p class="infos-desc"><?php echo nl2br($metaHome['description_gauche'][0]); ?></p>
+            </div>
+
+            <div class="infos-right">
+                <p class="infos-desc"><?php echo nl2br($metaHome['description_droite'][0]); ?></p>
+            </div>
+        </div>
+
+        <h2 class="title">Mes Services</h2>
+
+        <div class="card-container">
+            <div class="card one hover">
+                <!-- <div class="card-back">
+                    <div class="icon_card">
+                        <?php echo $svg['icon_service_1']; ?>
+                    </div>
+                
+                    <?php if(!empty($metaHome['titre_service_1'][0])){ ?>
+                        <h2 class="card-title"><?php echo ucfirst($metaHome['titre_service_1'][0]); ?></h2>
+                    <?php  } ?>
+                </div> -->
+
+                <div class="card-front">
+                    <?php if(!empty($metaHome['titre_service_1'][0])){ ?>
+                            <h2 class="card-title hover"><?php echo ucfirst($metaHome['titre_service_1'][0]); ?></h2>
+                    <?php  } ?>
+
+                    <div class="line-hover"></div>
+
+                    <ul class="desc-service">
+                        <li><?php echo ucfirst(nl2br($metaHome['description_service_1'][0])); ?></li>
+                    </ul>   
+                </div> 
+                                   
+            </div>
+        </div>
     </div>
 </div>
