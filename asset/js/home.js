8e6ceb94b87c23b2cbaded6f33dda199751f68c4
chris_tophe <nehligchris@live.fr>
2023-05-31 18:09:17 +0200

    responsive fini (need fix bug overflow wordpress)

--- asset/js/home.js
+++ asset/js/home.js
@@ -2,6 +2,8 @@ const header_logo = document.querySelector('#masthead .head-logo .logo');
 const hover_rs = document.querySelector('#masthead .hover-logo');
 const logo_rs = document.querySelector('#masthead .head-logo');
 
+const mobile_logo = document.querySelector('#masthead .mobile-head-logo .mobile-logo');
+const mobile_hover_rs = document.querySelector('#masthead .mobile-hover-logo');
 
 header_logo.addEventListener('pointerenter', function(){
     hover_rs.classList.remove('none');
@@ -10,3 +12,26 @@ header_logo.addEventListener('pointerenter', function(){
 logo_rs.addEventListener('pointerleave', function(){
     hover_rs.classList.add('none');
 });
+
+mobile_logo.addEventListener('click', function(){
+    if(mobile_hover_rs.classList.contains('none')){
+        mobile_hover_rs.classList.remove('none');
+    } else {
+        mobile_hover_rs.classList.add('none');
+    }
+});
+
+
+// Menu burger
+
+const burger_logo = document.querySelector('#masthead .mobile');
+const menu_burger = document.querySelector('#masthead .mobile-main-menu');
+const cross = document.querySelector('#masthead .mobile-main-menu .mobile-close');
+
+burger_logo.addEventListener('click', function(){
+    menu_burger.classList.add('open');
+})
+
+cross.addEventListener('click', function(){
+    menu_burger.classList.remove('open');
+})
---
+++ view/home/home-actus.php
@@ -0,0 +1,7 @@
+<?php global $metaHome; global $all; ?>
+
+<div id="actus">
+    <div class="wrap">
+        <h2 class="title">Dernières Actualités</h2>
+    </div>
+</div>
